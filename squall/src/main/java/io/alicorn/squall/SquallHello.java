6ae1cc0d348374a260262bdf3610f586215d8d0a
Brandon Sanders <brandon@alicorn.io>
2017-11-20 23:20:41 -0500

    Add squall for fun

---
+++ squall/src/main/java/io/alicorn/squall/SquallHello.java
@@ -0,0 +1,27 @@
+package io.alicorn.squall;
+
+import java.lang.Exception;
+import java.lang.Thread;
+
+import io.alicorn.squall.api.*;
+import io.alicorn.squall.api.http.*;
+
+public class SquallHello {
+    public static void main(String[] args) {
+        Squall squall = new Squall();
+        squall.setPort(3344);
+        squall.onGet("/hello", new HttpRequestHandler() {
+            public void handle(HttpRequestReader reader, HttpResponseWriter writer) throws Exception {
+                writer.send("Hello, world!");
+            }
+        });
+        squall.start();
+        while (true) { 
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
