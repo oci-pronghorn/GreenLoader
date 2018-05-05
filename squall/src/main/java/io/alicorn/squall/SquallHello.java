57f6cecccb9caad8029854be1faceb6bbdfd27dd
Brandon Sanders <brandon@alicorn.io>
2018-05-05 15:27:47 -0400

    Add endpoints to Squall.

--- squall/src/main/java/io/alicorn/squall/SquallHello.java
+++ squall/src/main/java/io/alicorn/squall/SquallHello.java
@@ -11,17 +11,22 @@ import io.alicorn.squall.api.http.*;
 public class SquallHello {
     public static void main(String[] args) {
         Squall squall = Squall.create(3307);
-        squall.onPost("/hello", new HttpRequestHandler() {
-            public void handle(HttpRequestReader reader, HttpResponseWriter writer) throws Exception {
-                JsonObject req = Json.parse(reader.getBody()).asObject();
-                JsonObject response = new JsonObject();
-                response.set("message", "Hello, " + req.getString("name", null) + "!");
-                response.set("happy", !req.getBoolean("happy", false));
-                response.set("age", req.getInt("age", 0) * 2);
-                writer.setContentType(HttpContentType.APPLICATION_JSON);
-                writer.send(response.toString());
-            }
+        squall.onPost("/hello", (reader, writer) -> {
+            JsonObject req = Json.parse(reader.getBody()).asObject();
+            JsonObject response = new JsonObject();
+            response.set("message", "Hello, " + req.getString("name", null) + "!");
+            response.set("happy", !req.getBoolean("happy", false));
+            response.set("age", req.getInt("age", 0) * 2);
+            writer.setContentType(HttpContentType.APPLICATION_JSON);
+            writer.send(response.toString());
         });
+
+        for (int i = 0; i < 19; i++) {
+            squall.onPost("/hello" + i, (req, res) -> {
+                res.send("You said hello to an alternate POST endpoint!");
+            });
+        }
+
         squall.start();
         while (true) { 
             try {
