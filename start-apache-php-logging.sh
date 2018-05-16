d1697faead858d81515008fc94531a631f18975a
Brandon Sanders <brandon@alicorn.io>
2018-05-16 15:26:07 -0400

    Change PHP to kill existing HTTPD before starting.

--- start-apache-php-logging.sh
+++ start-apache-php-logging.sh
@@ -3,5 +3,8 @@
 # Remove old logs.
 rm -rf php/logs/error_log
 
+# Kill existing processes.
+sudo killall httpd &> /dev/null
+
 # Start test.
 apachectl -d php -f httpd.conf -e info -D FOREGROUND -c 'Listen 3314' -D Logging
--- start-apache-php.sh
+++ start-apache-php.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# Kill existing processes.
+sudo killall httpd &> /dev/null
+
+# Start test.
 apachectl -d php -f httpd.conf -e info -D FOREGROUND -c 'Listen 3313'
