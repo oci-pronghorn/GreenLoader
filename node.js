0720b2cc92233953367934dac8ab6d2f049a40ee
Brandon Sanders <brandon@alicorn.io>
2018-05-22 15:13:11 -0400

    Update Node.js example to use the clustering module to support multiple simultaneous processors.

--- node.js
+++ node.js
@@ -4,53 +4,69 @@ var app = express();
 var fs = require('fs');
 var path = require('path');
 var morgan = require('morgan');
+var cluster = require('cluster');
 
-// Configuration
-var PORT = process.argv[2];
-var ENABLE_LOGGING = (process.argv[3] === "true");
+// If master, spawn workers.
+if (cluster.isMaster) {
 
-// Number of alternate endpoints.
-var ALTERNATE_ENDPOINTS_COUNT = 19;
+    // Count the machine's CPUs
+    var cpuCount = require('os').cpus().length;
 
-// Enable logging.
-if (ENABLE_LOGGING) {
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i++) {
+        cluster.fork();
+    }
 
-    // Add some tokens.	
-    morgan.token("reqHeaders", function (req, res) {	
-        return JSON.stringify(req.headers);	
-    });	
+// Otherwise, do work!
+} else {
+
+    // Configuration
+    var PORT = process.argv[2];
+    var ENABLE_LOGGING = (process.argv[3] === "true");
     
-    morgan.token("reqBody", function (req, res) {	
-        return JSON.stringify(req.body);
+    // Number of alternate endpoints.
+    var ALTERNATE_ENDPOINTS_COUNT = 19;
+    
+    // Enable logging.
+    if (ENABLE_LOGGING) {
+    
+        // Add some tokens.	
+        morgan.token("reqHeaders", function (req, res) {	
+            return JSON.stringify(req.headers);	
+        });	
+        
+        morgan.token("reqBody", function (req, res) {	
+            return JSON.stringify(req.body);
+        });
+    
+        // Setup log file.
+        var accessLogStream = fs.createWriteStream(path.join(__dirname, 'node-log.txt'), {flags: 'a'})
+    
+        // Enable Morgan.
+        app.use(morgan('ReqHeaders :reqHeaders ReqBody :reqBody', {stream: accessLogStream}));
+    }
+    
+    // Enable JSON body parsing.
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    
+    // Respond to POST request.
+    app.post('/hello', function (req, res) {
+        var json = req.body;
+        res.setHeader('Content-Type', 'application/json');
+        var response = {"message": 'Hello, ' + json.name + '!', "happy": !json.happy, "age": json.age * 2};
+        res.send(JSON.stringify(response));
     });
-
-    // Setup log file.
-    var accessLogStream = fs.createWriteStream(path.join(__dirname, 'node-log.txt'), {flags: 'a'})
-
-    // Enable Morgan.
-    app.use(morgan('ReqHeaders :reqHeaders ReqBody :reqBody', {stream: accessLogStream}));
-}
-
-// Enable JSON body parsing.
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Respond to POST request.
-app.post('/hello', function (req, res) {
-    var json = req.body;
-    res.setHeader('Content-Type', 'application/json');
-    var response = {"message": 'Hello, ' + json.name + '!', "happy": !json.happy, "age": json.age * 2};
-    res.send(JSON.stringify(response));
-});
-
-// Respond to POST request on other endpoints.
-for (var i = 0; i < ALTERNATE_ENDPOINTS_COUNT; i++) {
-    app.post('/hello' + i, function (req, res) {
-        res.send("You said hello to an alternate POST endpoint!");
+    
+    // Respond to POST request on other endpoints.
+    for (var i = 0; i < ALTERNATE_ENDPOINTS_COUNT; i++) {
+        app.post('/hello' + i, function (req, res) {
+            res.send("You said hello to an alternate POST endpoint!");
+        });
+    }
+    
+    // Begin listening.
+    app.listen(PORT, function() {
+        console.log("Now listening on " + PORT + " with logging " + (ENABLE_LOGGING ? "ENABLED" : "DISABLED"));
     });
 }
-
-// Begin listening.
-app.listen(PORT, function() {
-    console.log("Now listening on " + PORT + " with logging " + (ENABLE_LOGGING ? "ENABLED" : "DISABLED"));
-});
--- run-tracks.sh
+++ run-tracks.sh
@@ -3,45 +3,45 @@
 # Prevent SIGTERM from ending execution of tests.
 trap "echo Service attempted a SIGTERM." SIGTERM SIGKILL
 
-for TRACKS in 1 2 4 8 16; do
+for TRACKS in 1 2 4; do
 
     # Node
-    java -Xms10g -jar target/green-loader.jar -f load-configs/node.json -b 4 -t $TRACKS
-    java -Xms10g -jar target/green-loader.jar -f load-configs/node-logging.json -b 4 -t $TRACKS
+    java -Xms10g -jar target/green-loader.jar -f load-configs/node.json -b 6 -t $TRACKS
+    java -Xms10g -jar target/green-loader.jar -f load-configs/node-logging.json -b 6 -t $TRACKS
     
     # Jetty
-    java -Xms10g -jar target/green-loader.jar -f load-configs/jetty.json -b 4 -t $TRACKS
-    java -Xms10g -jar target/green-loader.jar -f load-configs/jetty-logging.json -b 4 -t $TRACKS
+    java -Xms10g -jar target/green-loader.jar -f load-configs/jetty.json -b 6 -t $TRACKS
+    java -Xms10g -jar target/green-loader.jar -f load-configs/jetty-logging.json -b 6 -t $TRACKS
     
     # Tomcat
-    java -Xms10g -jar target/green-loader.jar -f load-configs/tomcat.json -b 4 -t $TRACKS
-    java -Xms10g -jar target/green-loader.jar -f load-configs/tomcat-logging.json -b 4 -t $TRACKS
+    java -Xms10g -jar target/green-loader.jar -f load-configs/tomcat.json -b 6 -t $TRACKS
+    java -Xms10g -jar target/green-loader.jar -f load-configs/tomcat-logging.json -b 6 -t $TRACKS
 
     # Netty
-    java -Xms10g -jar target/green-loader.jar -f load-configs/netty.json -b 4 -t $TRACKS
+    java -Xms10g -jar target/green-loader.jar -f load-configs/netty.json -b 6 -t $TRACKS
 
     # Pronghorn
-    java -Xms10g -jar target/green-loader.jar -f load-configs/ph.json -b 4 -t $TRACKS
-    java -Xms10g -jar target/green-loader.jar -f load-configs/phl.json -b 4 -t $TRACKS
-    java -Xms10g -jar target/green-loader.jar -f load-configs/pht.json -b 4 -t $TRACKS
-    java -Xms10g -jar target/green-loader.jar -f load-configs/phlt.json -b 4 -t $TRACKS
+    java -Xms10g -jar target/green-loader.jar -f load-configs/ph.json -b 6 -t $TRACKS
+    java -Xms10g -jar target/green-loader.jar -f load-configs/phl.json -b 6 -t $TRACKS
+    java -Xms10g -jar target/green-loader.jar -f load-configs/pht.json -b 6 -t $TRACKS
+    java -Xms10g -jar target/green-loader.jar -f load-configs/phlt.json -b 6 -t $TRACKS
     
     # Green Lightning
-    java -Xms10g -jar target/green-loader.jar -f load-configs/gl.json -b 4 -t $TRACKS
-    java -Xms10g -jar target/green-loader.jar -f load-configs/gll.json -b 4 -t $TRACKS
-    java -Xms10g -jar target/green-loader.jar -f load-configs/glt.json -b 4 -t $TRACKS
-    java -Xms10g -jar target/green-loader.jar -f load-configs/gllt.json -b 4 -t $TRACKS
+    java -Xms10g -jar target/green-loader.jar -f load-configs/gl.json -b 6 -t $TRACKS
+    java -Xms10g -jar target/green-loader.jar -f load-configs/gll.json -b 6 -t $TRACKS
+    java -Xms10g -jar target/green-loader.jar -f load-configs/glt.json -b 6 -t $TRACKS
+    java -Xms10g -jar target/green-loader.jar -f load-configs/gllt.json -b 6 -t $TRACKS
 
     # PHP
-    java -Xms10g -jar target/green-loader.jar -f load-configs/php.json -b 4 -t $TRACKS
+    java -Xms10g -jar target/green-loader.jar -f load-configs/php.json -b 6 -t $TRACKS
     # Note: For PHP logging, we MUST delete its logs between runs; it just creates too much data.
     rm -rf php/logs/error_log
-    java -Xms10g -jar target/green-loader.jar -f load-configs/php-logging.json -b 4 -t $TRACKS
+    java -Xms10g -jar target/green-loader.jar -f load-configs/php-logging.json -b 6 -t $TRACKS
     
     # Micronaut
-    java -Xms10g -jar target/green-loader.jar -f load-configs/micronaut.json -b 4 -t $TRACKS
+    java -Xms10g -jar target/green-loader.jar -f load-configs/micronaut.json -b 6 -t $TRACKS
 
     # Play!
-    java -Xms10g -jar target/green-loader.jar -f load-configs/play.json -b 4 -t $TRACKS
+    java -Xms10g -jar target/green-loader.jar -f load-configs/play.json -b 6 -t $TRACKS
 
 done
